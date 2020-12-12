b41b4afd8a7a67e4550fb4f6f631141639df7f02
Rimuru Tempest <Kaktyskoder@gmail.com>
2020-12-12 20:49:44 +0400

    Fix code

---
+++ Assets/Scripts/AnimState.cs
@@ -0,0 +1,11 @@
+﻿
+/// <summary>
+/// Сосотояния анимаций.
+/// </summary>
+public enum AnimState : int
+{
+    Idle = 0,
+    Walk = 1,
+    Jump = 2,
+    Crouch = 3,
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,67 +1,61 @@
 ﻿using UnityEngine;
 
-/// <summary>
-/// Сосотояния анимаций.
-/// </summary>
-public enum AnimState : int
-{
-    Idle = 0,
-    Walk = 1,
-    Jump = 2,
-    Crouch = 3,
-}
-
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
-    public float jumpForce = 2.0f;
+    [Header("Set references in Inspector")]
+    public BoxCollider2D boxCollider1;      // Defalut
+    public BoxCollider2D boxCollider2;      // Crouch
 
-    public BoxCollider2D boxCollider1;      // enabled defalut or first  default collider
-    public BoxCollider2D boxCollider2;      // crouch
-    private SpriteRenderer sprite;
-    private Animator anim;
+    [Header("Set value in Inspector")]
+    public float jumpForce = 2.0f;
+    public float heightRestrictions = 0.5f;
+    public float moveSpeed = 1.0f;
+
     private Rigidbody2D rb2D;
-    private bool facingRight;
+    private Animator anim;
+    private SpriteRenderer sprite;
+
     private float moveInput;
+
 
     private void Start()
     {
-        sprite = GetComponent<SpriteRenderer>();
-        anim = GetComponent<Animator>();
         rb2D = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
     }
 
     private void FixedUpdate()
     {
         moveInput = Input.GetAxis("Horizontal");
 
-        rb2D.velocity = new Vector2(moveInput, rb2D.velocity.y);
+        rb2D.velocity = new Vector2(moveInput * moveSpeed, rb2D.velocity.y);
 
-        //if (facingRight == true && moveInput > 0)
-        //{
-        //    Flip();
-        //}
-        //else if (facingRight == false && moveInput < 0)
-        //{
-        //    Flip();
-        //}
-
-        if (moveInput == 0) // Если игрок ничего не делает.
+        if (moveInput == 0)
         {
             anim.SetInteger("Anim", (int)AnimState.Idle);
         }
 
-        if (moveInput > 0 || moveInput < 0)
-        {
-            anim.SetInteger("Anim", (int)AnimState.Walk);
-        }// Если игрок не стоит на месте, значит двигается (Walk)
-
         if (moveInput > 0)
         {
             sprite.flipX = false;
+            anim.SetInteger("Anim", (int)AnimState.Walk);
         }
-        else if(moveInput < 0)
+        else if (moveInput < 0)
         {
             sprite.flipX = true;
+            anim.SetInteger("Anim", (int)AnimState.Walk);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            if (transform.position.y > heightRestrictions) return;  // Временная решение.
+
+            Jump();
         }
 
         if (Input.GetKeyDown(KeyCode.S))
@@ -71,20 +65,15 @@ public class PlayerController : MonoBehaviour
 
         if (Input.GetKeyUp(KeyCode.S))
         {
-            CrouchOff();    // отпустили клавишу
-        }
-
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            Jump();
+            CrouchOff();
         }
     }
 
     private void Jump()
     {
-        anim.SetInteger("Anim", (int)AnimState.Jump);
-
         rb2D.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+
+        anim.SetInteger("Anim", (int)AnimState.Jump);
     }
 
     private void Crouch()
@@ -99,15 +88,5 @@ public class PlayerController : MonoBehaviour
     {
         boxCollider1.enabled = true;
         boxCollider2.enabled = false;
-    }
-
-    private void Flip()
-    {
-        facingRight = !facingRight;
-
-        Vector3 scaler = transform.localScale;
-        scaler.x *= -1;
-
-        transform.localScale = scaler;
     }
 }
