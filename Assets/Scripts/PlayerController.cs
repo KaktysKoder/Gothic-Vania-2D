c4cf041390651ec4ecd67f0fad2a3c923a93ae38
Rimuru Tempest <Kaktyskoder@gmail.com>
2020-12-12 17:07:49 +0400

    Test animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,20 +1,80 @@
 ﻿using UnityEngine;
 
+/// <summary>
+/// Сосотояния анимаций.
+/// </summary>
+public enum AnimState : int
+{
+    Idle = 0,
+    Walk = 1,
+    Jump = 2,
+    Crouch = 3,
+}
+
 public class PlayerController : MonoBehaviour
 {
     public float jumpForce = 2.0f;
+
+    public BoxCollider2D boxCollider1;      // enabled defalut or first  default collider
+    public BoxCollider2D boxCollider2;      // crouch
+    private SpriteRenderer sprite;
+    private Animator anim;
     private Rigidbody2D rb2D;
+    private bool facingRight;
+    private float moveInput;
 
     private void Start()
     {
+        sprite = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
         rb2D = GetComponent<Rigidbody2D>();
     }
 
     private void FixedUpdate()
     {
-        rb2D.velocity = new Vector2(Input.GetAxis("Horizontal"), rb2D.velocity.y);
+        moveInput = Input.GetAxis("Horizontal");
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        rb2D.velocity = new Vector2(moveInput, rb2D.velocity.y);
+
+        //if (facingRight == true && moveInput > 0)
+        //{
+        //    Flip();
+        //}
+        //else if (facingRight == false && moveInput < 0)
+        //{
+        //    Flip();
+        //}
+
+        if (moveInput == 0) // Если игрок ничего не делает.
+        {
+            anim.SetInteger("Anim", (int)AnimState.Idle);
+        }
+
+        if (moveInput > 0 || moveInput < 0)
+        {
+            anim.SetInteger("Anim", (int)AnimState.Walk);
+        }// Если игрок не стоит на месте, значит двигается (Walk)
+
+        if (moveInput > 0)
+        {
+            sprite.flipX = false;
+        }
+        else if(moveInput < 0)
+        {
+            sprite.flipX = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Crouch();
+        }
+
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            CrouchOff();    // отпустили клавишу
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
         {
             Jump();
         }
@@ -22,6 +82,32 @@ public class PlayerController : MonoBehaviour
 
     private void Jump()
     {
+        anim.SetInteger("Anim", (int)AnimState.Jump);
+
         rb2D.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+    }
+
+    private void Crouch()
+    {
+        anim.SetInteger("Anim", (int)AnimState.Crouch);
+
+        boxCollider1.enabled = false;
+        boxCollider2.enabled = true;
+    }
+
+    private void CrouchOff()
+    {
+        boxCollider1.enabled = true;
+        boxCollider2.enabled = false;
+    }
+
+    private void Flip()
+    {
+        facingRight = !facingRight;
+
+        Vector3 scaler = transform.localScale;
+        scaler.x *= -1;
+
+        transform.localScale = scaler;
     }
 }
