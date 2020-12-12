66d6b52f401282805adfbbab8eb24955e8400220
Rimuru Tempest <Kaktyskoder@gmail.com>
2020-12-12 23:21:27 +0400

    Camera movement

---
+++ Assets/Scripts/WatchPlayer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class WatchPlayer : MonoBehaviour
+{
+    public float moveSpeedCamera = 1.0f;
+
+    private Transform player;
+    private Vector3 playerVector;
+
+    private void Start()
+    {
+        player = GameObject.Find("Player").transform;
+    }
+
+    private void FixedUpdate()
+    {
+        playerVector = player.position;
+        playerVector.z = -10;
+
+        transform.position = Vector3.Lerp(transform.position, playerVector, Time.deltaTime * moveSpeedCamera);
+    }
+}
